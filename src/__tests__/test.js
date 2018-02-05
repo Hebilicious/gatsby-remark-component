70d4cc22a6a8f68f7f653255b687723fc429b8be
Hebilicious <xsh4k3@gmail.com>
2018-02-05 21:11:03 +0100

    Updating tests.

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,5 +1,5 @@
 const Remark = require(`remark`)
-const plugin = require(`../index`)
+const plugin = require(`../../lib`)
 
 const markdown = "Text test\n<my-component></my-component>\n###Test"
 const markdowns = [markdown]
@@ -15,12 +15,23 @@ describe(`remark component plugin`, () => {
     })
   })
 
-  it(`Should set a component parent type to div in the AST.`, () => {
+  it(`Set a component with components parent type to div in the AST.`, () => {
     let remark = new Remark()
 
     markdowns.forEach(md => {
       let markdownAST = remark.parse(md)
       plugin({ markdownAST }, { ...options })
+      expect(markdownAST.children[0]).toMatchObject({ type: "div" })
+      expect(markdownAST).toMatchSnapshot()
+    })
+  })
+
+  it(`Should set a component without components or options parent type to div in the AST.`, () => {
+    let remark = new Remark()
+
+    markdowns.forEach(md => {
+      let markdownAST = remark.parse(md)
+      plugin({ markdownAST }, {})
       expect(markdownAST.children[0]).toMatchObject({ type: "div" })
       expect(markdownAST).toMatchSnapshot()
     })
